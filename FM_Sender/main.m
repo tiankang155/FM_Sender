2f4fcdf0b389e41625528069809904f5cfaf33ec
yarui <yarui@TiandeMacBook-Pro.local>
2016-11-16 14:38:47 +0800

    Initial Commit

---
+++ FM_Sender/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FM_Sender
+//
+//  Created by yarui on 2016/11/16.
+//  Copyright © 2016年 GeMei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
